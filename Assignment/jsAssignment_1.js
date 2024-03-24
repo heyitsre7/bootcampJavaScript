0511dabfdf4ee33c3abb3b4da6d22e6e66949e1a
heyitsre7 <140890088+heyitsre7@users.noreply.github.com>
2024-03-24 09:27:34 +0000

    complete

---
+++ Assignment/jsAssignment_1.js
@@ -0,0 +1,14 @@
+let a=20;
+let b=5;
+
+let sum = a + b;
+console.log(sum);
+
+let diff = a - b;
+console.log(diff);
+
+let pro = a * b;
+console.log(pro);
+
+let modul = a % b;
+console.log(modul); 
---
+++ Assignment/jsAssignment_2.js
@@ -0,0 +1,3 @@
+let a = 50;
+let b = 2;
+console.log(math.floor(a/b));
