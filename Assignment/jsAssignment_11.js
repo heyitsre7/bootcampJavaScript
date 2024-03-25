1ad45ddc836213919b4b37945fb08a736ea3c3dc
heyitsre7 <140890088+heyitsre7@users.noreply.github.com>
2024-03-25 08:29:12 +0000

    done

---
+++ Assignment/jsAssignment_11.js
@@ -0,0 +1,18 @@
+const num = 19;
+let i=1;
+let c=0;
+while(i<=num)
+{
+    if(num%i===0)
+    {
+        c++;
+    }
+    i++;
+}
+if(count==2)
+{
+    console.log("This is a Prime Number");
+}
+else {
+    console.log("This is not a Prime Number");
+}
---
+++ Assignment/jsAssignment_12.js
@@ -0,0 +1,9 @@
+// convert celcius to fahrenheit
+let ct=30;
+let ft=(ct*9/5)+32;
+console.log(ft);
+
+// Convert Fahrenheit to Celsius
+ft = 86;
+ct = (ft-32)*5/9;
+console.log(ct);
