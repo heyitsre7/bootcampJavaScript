b916feb2ff7e82f957d24f87ce6e30ba46ae6f02
heyitsre7 <140890088+heyitsre7@users.noreply.github.com>
2024-04-29 10:39:00 +0530

    Add files via upload

---
+++ test5.js
@@ -0,0 +1,68 @@
+// // 1) Sum of Array Elements: Write a function to calculate the sum of all elements in an array of integers.
+
+// // const arr = [1,2,3,4,5,6];
+// // function sum (arr){
+// //     let add=0;
+// //     for(let index = 0; index < arr.length; index++) {
+// //             add= add+arr[index];
+// //         }
+// //     return add;
+// // }
+// // console.log(sum(arr));
+
+
+// // 2) Average of Array Elements: Write a function to calculate the average of all elements in an array of integers.
+
+// // const arr = [1,2,3,4,5,6];
+// // function avg (arr){
+// //     let add=0;
+// //     for(let index = 0; index < arr.length; index++) {
+// //             add= add+arr[index];
+// //         }
+// //     return add/arr.length;
+// // }
+// // console.log(avg(arr));
+
+// // 3) Find Maximum Element: Write a function to find the maximum element in an array of integers.
+
+// // const arr = [4,5,2,7,8,1];
+// // function findMax(array) {
+// //     return Math.max(...array);
+// // }
+// // console.log(findMax(arr));
+
+// // 4) Find Minimum Element: Write a function to find the minimum element in an array of integers.
+
+// // const arr = [4,5,2,7,8,1];
+// // function findMin(array) {
+// //     return Math.min(...array);
+// // }
+// // console.log(findMin(arr));
+
+// //5) Array Search: Write a function to search for a given element in an array and return its index if found, otherwise return -1.
+// // const arr = [3,5,6,3,7,8,9];
+// // const num = 8;
+// // function search(array, num){
+// //     if(arr.includes(num)){
+// //         return arr.indexOf(num);
+// //     }
+// //     else{
+// //         return -1;
+// //     }
+// // }
+// // console.log(search(arr,num ));
+
+// // 6) Array Sorting: Write a function to sort an array of integers in non-decreasing order (ascending).
+
+
+
+
+
+
+
+
+
+
+
+
+
